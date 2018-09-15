6b484ccdd4016e1c4f9c64c30278ac67c0041f69
Gary A. Stafford <garystafford@rochester.rr.com>
2018-09-15 18:15:18 -0400

    Fully working with Stackdriver winston logging

---
+++ helper.js
@@ -0,0 +1,136 @@
+// author: Gary A. Stafford
+// site: https://programmaticponderings.com
+// license: MIT License
+
+'use strict';
+
+/* CONSTANTS AND GLOBAL VARIABLES */
+
+const {
+    dialogflow,
+    BasicCard,
+    SimpleResponse,
+} = require('actions-on-google');
+
+const app = dialogflow({debug: true});
+app.middleware(conv => {
+    conv.hasScreen =
+        conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT');
+    conv.hasAudioPlayback =
+        conv.surface.capabilities.has('actions.capability.AUDIO_OUTPUT');
+});
+
+const SEARCH_API_HOSTNAME = process.env.SEARCH_API_HOSTNAME;
+const SEARCH_API_PORT = process.env.SEARCH_API_PORT;
+const SEARCH_API_ENDPOINT = process.env.SEARCH_API_ENDPOINT;
+
+const Request = require("request");
+
+const winston = require('winston');
+const Logger = winston.Logger;
+const Console = winston.transports.Console;
+const {LoggingWinston} = require('@google-cloud/logging-winston');
+const loggingWinston = new LoggingWinston();
+const logger = new Logger({
+    level: 'info', // log at 'info' and above
+    transports: [
+        new Console(),
+        loggingWinston,
+    ],
+});
+
+
+/* HELPER FUNCTIONS */
+
+module.exports = class Helper {
+    /**
+     * Returns an collection of ElasticsearchPosts objects based on a topic
+     * @param postTopic topic to search for
+     * @param responseSize
+     * @returns {Promise<any>}
+     */
+    getPostsByTopic(postTopic, responseSize = 1) {
+        return new Promise((resolve, reject) => {
+
+            const SEARCH_API_RESOURCE = `dismax-search?value=${postTopic}&start=0&size=${responseSize}&minScore=1`;
+            const SEARCH_API_URL = `http://${SEARCH_API_HOSTNAME}:${SEARCH_API_PORT}/${SEARCH_API_ENDPOINT}/${SEARCH_API_RESOURCE}`;
+            logger.info(`getPostsByTopic API URL: ${SEARCH_API_URL}`);
+
+            let posts = {};
+
+            Request.get(SEARCH_API_URL, (error, response, body) => {
+                if (error) {
+                    logger.error(`Error: ${error}`);
+                    reject(`Sorry, I could find any posts about ${postTopic}.`)
+                }
+                posts = JSON.parse(body);
+                posts = posts.ElasticsearchPosts;
+                resolve(posts);
+            });
+        });
+    }
+
+    /**
+     * Returns a single result based in the Post ID
+     * @param postId ID of the Post to search for
+     * @returns {Promise<any>}
+     */
+    getPostById(postId) {
+        return new Promise((resolve, reject) => {
+            const SEARCH_API_RESOURCE = `${postId}`;
+            const SEARCH_API_URL = `http://${SEARCH_API_HOSTNAME}:${SEARCH_API_PORT}/${SEARCH_API_ENDPOINT}/${SEARCH_API_RESOURCE}`;
+            logger.info(`getPostById API URL: ${SEARCH_API_URL}`);
+
+            let post = {};
+
+            Request.get(SEARCH_API_URL, (error, response, body) => {
+                if (error) {
+                    logger.error(`Error: ${error}`);
+                    reject(`Sorry, I could find post ${postId}.`)
+                }
+                post = JSON.parse(body);
+                post = post.ElasticsearchPosts;
+                logger.info(`getPostById Post: ${post}`);
+                resolve(post);
+            });
+        });
+    }
+
+    convertDate(dateString) {
+        let post_date = new Date(dateString);
+        let options = {year: 'numeric', month: 'long', day: 'numeric'};
+        return post_date.toLocaleDateString('en-EN', options);
+    }
+
+    topicNotFound(conv, postTopic) {
+        const NOT_FOUND_TEXT = `Sorry, I can't find any posts for the topic '${postTopic}'`;
+
+        conv.ask(new SimpleResponse({
+            speech: NOT_FOUND_TEXT,
+            text: NOT_FOUND_TEXT,
+        }));
+
+        if (conv.hasScreen) {
+            conv.ask(new BasicCard({
+                title: `Topic Not Found`,
+                text: NOT_FOUND_TEXT,
+            }));
+        }
+    }
+
+    postIdNotFound(conv, postId) {
+        const NOT_FOUND_TEXT = `Sorry, I can't find post ID ${postId}'`;
+
+        conv.ask(new SimpleResponse({
+            speech: NOT_FOUND_TEXT,
+            text: NOT_FOUND_TEXT,
+        }));
+
+        if (conv.hasScreen) {
+            conv.ask(new BasicCard({
+                title: `Post Not Found`,
+                text: NOT_FOUND_TEXT,
+            }));
+        }
+    }
+};
--- index.js
+++ index.js
@@ -6,6 +6,9 @@
 
 /* CONSTANTS AND GLOBAL VARIABLES */
 
+const Helper = require('./helper');
+let helper = new Helper();
+
 const {
     dialogflow,
     Button,
@@ -14,11 +17,9 @@ const {
     SimpleResponse,
     List
 } = require('actions-on-google');
-const Request = require("request");
 const functions = require('firebase-functions');
-const {createLogger, format, transports} = require('winston');
-const app = dialogflow({debug: true});
 
+const app = dialogflow({debug: true});
 app.middleware(conv => {
     conv.hasScreen =
         conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT');
@@ -26,22 +27,9 @@ app.middleware(conv => {
         conv.surface.capabilities.has('actions.capability.AUDIO_OUTPUT');
 });
 
-// Setup Logging
-const logger = createLogger({
-    level: 'info',
-    format: format.json(),
-    transports: [
-        new transports.Console()
-    ]
-});
-
 const SUGGESTION_1 = 'tell me about Docker';
 const SUGGESTION_2 = 'help';
 const SUGGESTION_3 = 'cancel';
-
-const SEARCH_API_HOSTNAME = process.env.SEARCH_API_HOSTNAME;
-const SEARCH_API_PORT = process.env.SEARCH_API_PORT;
-const SEARCH_API_ENDPOINT = process.env.SEARCH_API_ENDPOINT;
 
 /* INTENT HANDLERS */
 
@@ -90,17 +78,17 @@ app.intent('Fallback Intent', conv => {
 app.intent('Find Post Intent', async (conv, {topic}) => {
     let postTopic = topic.toString();
 
-    let posts = await getPosts(postTopic, 1);
+    let posts = await helper.getPostsByTopic(postTopic, 1);
 
-    if (posts.length < 1) {
-        topicNotFound(conv, postTopic);
+    if (posts !== undefined && posts.length < 1) {
+        helper.topicNotFound(conv, postTopic);
         return;
     }
 
     let post = posts[0];
-    let formattedDate = convertDate(post.post_date);
-    const POST_SPOKEN = `The top result for '${postTopic}' is the post, '${post.post_title}', published ${formattedDate}`;
-    const POST_TEXT = `Description: _${post.post_excerpt}_  \nPublished: ${formattedDate}`;
+    let formattedDate = helper.convertDate(post.post_date);
+    const POST_SPOKEN = `The top result for '${postTopic}' is the post, '${post.post_title}', published ${formattedDate}, with a relevancy score of ${post._score.toFixed(2)}`;
+    const POST_TEXT = `Description: ${post.post_excerpt}  \nPublished: ${formattedDate}  \nScore: ${post._score.toFixed(2)}`;
 
     conv.ask(new SimpleResponse({
         speech: POST_SPOKEN,
@@ -125,10 +113,10 @@ app.intent('Find Post Intent', async (conv, {topic}) => {
 app.intent('Find Multiple Posts Intent', async (conv, {topic}) => {
     let postTopic = topic.toString();
     let postCount = 6;
-    let posts = await getPosts(postTopic, postCount);
+    let posts = await helper.getPostsByTopic(postTopic, postCount);
 
-    if (posts.length < 1) {
-        topicNotFound(conv, postTopic);
+    if (posts !== undefined && posts.length < 1) {
+        helper.topicNotFound(conv, postTopic);
         return;
     }
 
@@ -143,7 +131,7 @@ app.intent('Find Multiple Posts Intent', async (conv, {topic}) => {
     posts.forEach(function (post) {
         itemsArray[post.ID] = {
             title: `Post ID ${post.ID}`,
-            description: post.post_title,
+            description: `${post.post_title}  \nScore: ${post._score.toFixed(2)}`,
         };
     });
 
@@ -159,16 +147,17 @@ app.intent('Find Multiple Posts Intent', async (conv, {topic}) => {
 
 app.intent('Find By ID Intent', async (conv, {topic}) => {
     let postId = topic.toString();
-    let post = await getPostById(postId);
+
+    let post = await helper.getPostById(postId);
 
     if (post === undefined) {
-        topicNotFound(conv, postId);
+        helper.postIdNotFound(conv, postId);
         return;
     }
 
-    let formattedDate = convertDate(post.post_date);
+    let formattedDate = helper.convertDate(post.post_date);
     const POST_SPOKEN = `Okay, I found that post`;
-    const POST_TEXT = `Description: _${post.post_excerpt}_  \nPublished: ${formattedDate}`;
+    const POST_TEXT = `Description: ${post.post_excerpt}  \nPublished: ${formattedDate}  \nScore: ${post._score.toFixed(2)}`;
 
     conv.ask(new SimpleResponse({
         speech: POST_SPOKEN,
@@ -191,22 +180,18 @@ app.intent('Find By ID Intent', async (conv, {topic}) => {
 });
 
 app.intent('Option Intent', async (conv, params, option) => {
-    let postId;
+    let postId = option.toString();
 
-    if (option) {
-        postId = option;
-    }
-
-    let post = await getPostById(postId);
+    let post = await helper.getPostById(postId);
 
     if (post === undefined) {
-        topicNotFound(conv, postId);
+        helper.postIdNotFound(conv, postId);
         return;
     }
 
-    let formattedDate = convertDate(post.post_date);
+    let formattedDate = helper.convertDate(post.post_date);
     const POST_SPOKEN = `Sure, here's that post`;
-    const POST_TEXT = `Description: _${post.post_excerpt}_  \nPublished: ${formattedDate}`;
+    const POST_TEXT = `Description: ${post.post_excerpt}  \nPublished: ${formattedDate}  \nScore: ${post._score.toFixed(2)}`;
 
     conv.ask(new SimpleResponse({
         speech: POST_SPOKEN,
@@ -227,78 +212,6 @@ app.intent('Option Intent', async (conv, params, option) => {
         conv.ask(new Suggestions([SUGGESTION_1, SUGGESTION_2, SUGGESTION_3]));
     }
 });
-
-
-/* HELPER FUNCTIONS */
-
-function getPosts(postTopic, responseSize = 1) {
-    return new Promise((resolve, reject) => {
-
-        const SEARCH_API_RESOURCE = `dismax-search?value=${postTopic}&start=0&size=${responseSize}&minScore=1`;
-        const SEARCH_API_URL = `http://${SEARCH_API_HOSTNAME}:${SEARCH_API_PORT}/${SEARCH_API_ENDPOINT}/${SEARCH_API_RESOURCE}`;
-
-        let posts = {};
-
-        Request.get(SEARCH_API_URL, (error, response, body) => {
-            if (error) {
-                logger.log({
-                    level: 'error',
-                    message: `Error: ${error}`
-                });
-                reject(`Sorry, I could find any posts about ${postTopic}.`)
-            }
-            posts = JSON.parse(body);
-            posts = posts.ElasticsearchPosts;
-            resolve(posts);
-        });
-    });
-}
-
-function getPostById(postId) {
-    return new Promise((resolve, reject) => {
-
-        const SEARCH_API_RESOURCE = `${postId}`;
-        const SEARCH_API_URL = `http://${SEARCH_API_HOSTNAME}:${SEARCH_API_PORT}/${SEARCH_API_ENDPOINT}/${SEARCH_API_RESOURCE}`;
-        let posts = {};
-
-        Request.get(SEARCH_API_URL, (error, response, body) => {
-            if (error) {
-                logger.log({
-                    level: 'error',
-                    message: `Error: ${error}`
-                });
-                reject(`Sorry, I could find any posts about ${postTopic}.`)
-            }
-            posts = JSON.parse(body);
-            resolve(posts);
-        });
-    });
-}
-
-function convertDate(dateString) {
-    let post_date = new Date(dateString);
-    let options = {year: 'numeric', month: 'long', day: 'numeric'};
-    return post_date.toLocaleDateString('en-EN', options);
-}
-
-function topicNotFound(conv, postTopic) {
-    const NOT_FOUND_TEXT = `Sorry, I cannot find any posts for the topic '${postTopic}'`;
-
-    conv.ask(new SimpleResponse({
-        speech: NOT_FOUND_TEXT,
-        text: NOT_FOUND_TEXT,
-    }));
-
-    if (conv.hasScreen) {
-        conv.ask(new BasicCard({
-            title: `Topic Not Found`,
-            text: NOT_FOUND_TEXT,
-        }));
-
-        conv.ask(new Suggestions([SUGGESTION_1, SUGGESTION_2, SUGGESTION_3]));
-    }
-
-}
 
 
 /* ENTRY POINT */
